b76a7b794d8421d8af52aa9507e0ba2acf3d5e9e
Ayushkumarsinha123 <itsharshit0005@gmail.com>
2026-03-12 17:22:42 +0530

    feat:implemented signup page UI with material UI components

--- client/src/pages/SignupPage.jsx
+++ client/src/pages/SignupPage.jsx
@@ -1,5 +1,9 @@
 import { useState } from "react";
 import { signupUser } from "../services/authService";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import FoodPanda from "../assets/FoodPanda.jfif"
+import ShoppingCartRoundedIcon from "@mui/icons-material/ShoppingCartRounded";
 
 function Signup() {
     const [form, setForm ] = useState({
@@ -29,30 +33,51 @@ function Signup() {
       }
     }
     return (
-      <div>
-        <h2>Signup</h2>
+      <div className="min-h-screen flex item-center justify-center bg-gray-100 p-4">
+      <div className="bg-white rounded-2xl shadow-xl flex w-full max-w-4xl overflow-hidden">
+        <div className="w-full md:w-1/2 p-8 sm:p-12 flex flex-col justify-center">
+        <h2 className="text-3xl font-bold mb-6 text-center">Signup</h2>
 
-        <form onSubmit={handleSubmit}>
-          <input
-          type="email"
-          name="email"
-          placeholder="Email"
-          onChange={handleChange}/>
+        <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+          <TextField
+                  label="Name"
+                  name="name"
+                  value={form.name}
+                  onChange={handleChange}
+                  fullWidth
+                />
 
-          <input 
-          type="text"
-          name="name"
-          placeholder="Name"
-          onChange={handleChange}/>
+                <TextField
+                  label="Email"
+                  name="email"
+                  type="email"
+                  value={form.email}
+                  onChange={handleChange}
+                  fullWidth
+                />
 
-          <input 
-          type="password"
-          name="password"
-          placeholder="Password" 
-          onChange={handleChange}/>
-
-          <button type="submit">signup</button>
+                <TextField
+                  label="Password"
+                  name="password"
+                  type="password"
+                  value={form.password}
+                  onChange={handleChange}
+                  fullWidth
+                />
+          
+          <Button type="submit" variant="contained" >
+  SIGN UP
+</Button>
         </form>
+        </div>
+        <div className="hidden md:block w-1/2 bg-gray-200">
+        <img 
+        src={FoodPanda}
+        alt="food panda"
+        className="w-full h-full object-cover"
+        />
+      </div>
+      </div>
       </div>
     )
 }

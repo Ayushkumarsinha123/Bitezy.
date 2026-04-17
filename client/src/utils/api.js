00966d67b510ddc5381e934faf3be9432a3868c9
Ayushkumarsinha123 <itsharshit0005@gmail.com>
2026-04-17 19:18:58 +0530

    fix: configure client API URL for deployment

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:3000";
 
 const API = axios.create({
-  baseURL : "http://localhost:3000/api/"
+  baseURL: `${API_URL}/api/`
 });
 
 export default API;
